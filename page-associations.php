e1aaafa3259315b7cbc28ee2be365b19986981bf
Frost <cpaccione77@gmail.com>
2016-03-01 01:37:22 -0600

    Theme updates

--- page-associations.php
+++ page-associations.php
@@ -2,7 +2,7 @@
 
 /*
 
-Template Name: Associations Template
+Template Name: Associations
 
 */
 
@@ -22,16 +22,18 @@ Template Name: Associations Template
         </div>
       </div>
 
+      <div class="row">
+      <div class="col-md-12">
   <!-- Nav tabs -->
   <ul class="nav nav-pills nav-justified" role="tablist">
-    <li role="presentation"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Health &amp; Benefits</a></li>
-    <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Human Resources</a></li>
-    <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Retirment Services</a></li>
+    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">HEALTH &amp; BENEFITS</a></li>
+    <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">HUMAN RESOURCES</a></li>
+    <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">RETIREMENT SERVICES</a></li>
   </ul>
 
   <!-- Tab panes -->
   <div class="tab-content">
-    <div role="tabpanel" class="tab-pane fade" id="home">
+    <div role="tabpanel" class="tab-pane fade in active" id="home">
 
       <?php if( have_rows('health_and_benefits') ): ?>
 
@@ -46,18 +48,64 @@ Template Name: Associations Template
 
           ?>
 
-          <h3><a href="<?php echo $link; ?>"><?php echo $hb_title; ?></a></h3>
+          <h3><a href="<?php echo $hb_link; ?>" target="_blank"><?php echo $hb_title; ?></a></h3>
           <p><?php echo $hb_summary; ?></p>
 
         <?php endwhile; ?>
 
       <?php endif;?>
 
-      <h3>Society of Actuaries (SOA)</h3>
-      <p>An educational, research and professional membership organization dedicated to serving 20,000 actuarial members and the public in the United States and Canada. The SOA’s vision is for actuaries to be the leading professionals in the measurement and management of risk.</p>
     </div>
-    <div role="tabpanel" class="tab-pane fade" id="profile">.</div>
-    <div role="tabpanel" class="tab-pane fade" id="messages">...</div>
+    <div role="tabpanel" class="tab-pane fade" id="profile">
+
+      <?php if( have_rows('human_resources') ): ?>
+
+          <?php while( have_rows('human_resources') ): the_row(); 
+
+          //variables
+          $hr_link = get_sub_field('link');
+          $hr_title = get_sub_field('link_title');
+          $hr_summary = get_sub_field('link_summary');
+
+
+
+          ?>
+
+          <h3><a href="<?php echo $hr_link; ?>" target="_blank"><?php echo $hr_title; ?></a></h3>
+          <p><?php echo $hr_summary; ?></p>
+
+        <?php endwhile; ?>
+
+      <?php endif;?>
+
+    </div>
+
+    <div role="tabpanel" class="tab-pane fade" id="messages">
+      
+      <?php if( have_rows('retirement_services') ): ?>
+
+          <?php while( have_rows('retirement_services') ): the_row(); 
+
+          //variables
+          $rt_link = get_sub_field('link');
+          $rt_title = get_sub_field('link_title');
+          $rt_summary = get_sub_field('link_summary');
+
+
+
+          ?>
+
+          <h3><a href="<?php echo $rt_link; ?>" target="_blank"><?php echo $rt_title; ?></a></h3>
+          <p><?php echo $rt_summary; ?></p>
+
+        <?php endwhile; ?>
+
+      <?php endif;?>
+
+    </div>
+    
+  </div>
+  </div>
   </div>
 
 </div>
--- page-search-process.php
+++ page-search-process.php
@@ -15,20 +15,66 @@ Template Name: Search Process
 
 <section class="search-process">
   <div class="container">
-      <div class="row">
+  
+  <div class="row">
         <div class="col-md-12 text-center">
           <h2>SEARCH PROCESS</h2>
         </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12">
-          <div class="green-copy-box">
-            <div class="green-copy-wrap">
-              <p>Our experiences indicates that the prospect of a successful hire is significantly enhanced when we collaborate with our clients in a well thought out and carefully defined process of search execution.</p>
-            </div>
-      </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-12">
+      <div class="green-copy-box">
+        <div class="green-copy-wrap">
+        <p>Our experiences indicates that the prospect of a successful hire is significantly enhanced when we collaborate with our clients in a well thought out and carefully defined process of search execution.</p>
+        </div>
       </div>
     </div>
+  </div>
+
+  <div class="row">
+    <div class="search-process-wrap">
+      <div class="col-md-12">
+
+          <h3>Week One – Position Description, Research and Recruitment</h3>
+          <p><i class="fa fa-check-square-o"></i>In collaboration with our client, Kelly & Company prepares a position description to serve as the roadmap for the search.</p>
+          <p><i class="fa fa-check-square-o"></i>A target list of candidate sources is prepared and research and recruitment begins.</p>
+
+          <h3>Week Two to Four – Recruitment and Assessment</h3>
+          <p><i class="fa fa-check-square-o"></i>Potential candidates are screened by telephone and appropriate candidates are interviewed in person to assess their suitability.</p>
+
+          <p><i class="fa fa-check-square-o"></i>Once we identify an appropriate candidate, we prepare a profile that provides in-depth information as to the candidate’s circumstances and experience.</p>
+
+          <p><i class="fa fa-check-square-o"></i>Research and recruiting continues to identify additional candidates.</p>
+
+          <p><i class="fa fa-check-square-o"></i>Search progress is discussed with our client on a weekly basis.</p>
+
+          <h3>Week Five – Initial Search Meetings</h3>
+
+          <p><i class="fa fa-check-square-o"></i>We present our initial candidates along with pertinent market data uncovered during the course of our research and recruiting. We then begin scheduling client/candidate interviews.</p>
+
+          <p><i class="fa fa-check-square-o"></i>Research, recruiting, and assessment to identify additional appropriate candidates continues.</p>
+
+          <h3>Week Six and Seven – Client Interviews and Follow up</h3>
+          
+          <p><i class="fa fa-check-square-o"></i>Client candidates interviews are scheduled, feedback is collected and communicated, issues are discussed and resolved, and appropriate and interested candidates are advanced to the next round.</p>
+
+          <p><i class="fa fa-check-square-o"></i>Research, recruiting, and assessment to identify additional appropriate candidates continues.</p>
+
+          <h3>Week Eight to Closure – Client Interviews/Referencing/Structuring the Offer and Closing</h3>
+
+          <p><i class="fa fa-check-square-o"></i>As the focus narrows to one or two individuals, Kelly & Company performs the final stages of due diligence. In addition to the confirmation of information regarding current employment and compensation, we perform extensive reference checks and present our findings in writing.</p>
+
+          <p><i class="fa fa-check-square-o"></i>Throughout the process, we are in constant contact with the candidates to ensure their continued interest in the opportunity and to identify and resolve any concerns that they may have.</p>
+
+          <p><i class="fa fa-check-square-o"></i>We assist in structuring the offer and closing the hire.</p>
+
+          <p><i class="fa fa-check-square-o"></i>We closely monitor the selected candidate’s resignation and transition to our client.</p>
+
+      </div>
+    </div>
+  </div>
+    
   </div>
 </section>
 
