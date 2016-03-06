e9e2c9d33dc7c85cd3b910e21afe3eb8e26e44f1
Frost <cpaccione77@gmail.com>
2016-03-06 17:47:17 -0600

    Home page updates, and CSS clean up

--- page-home.php
+++ page-home.php
@@ -8,25 +8,16 @@ Template Name: Home Page
 
 ?>
 
-
 <?php get_header(); ?>
 
 <section class="nyc-header">
     <div class="container home-page">
       <div class="row">
-
-            <div class="col-xs-12 col-md-8 clear-boot-padding">
-              <div class="green-box">
-                <p>
-                  Identification<br>
-                  Recruitment<br>
-                  Selection<br>
-                  Assimilation<br>
-                  Retention
-                </p>
-              </div>
-            </div>
-
+        <div class="col-xs-12 col-md-8 clear-boot-padding">
+          <div class="green-box">
+            <p><?php the_field('green_box'); ?></p>
+          </div>
+        </div>
       </div>
     </div>
 </section>
@@ -35,19 +26,12 @@ Template Name: Home Page
     <div class="container">
       <div class="row">
         <div class="col-md-12">
-
           <div class="home-page-copy center-block">
-            
-            <h2>ABOUT KELLY &amp; COMPANY</h2>
-
-            <p>Established in 1996, Kelly & Company is a retained executive search firm with expertise in the identification, recruitment, selection, assimilation, and retention of senior talent within Retirement Services, Employee Benefits Consulting &amp; Administration, Human Resources Consulting & Administration, and Corporate Human Resources.</p>
-
-            <p>Our clients include a select group of Financial Services firms in the Retirements Services business, Employee Benefits Consulting & Administration firms, Human Resources Consulting & Administration firms, and Fortune 500 corporations. These clients appreciate the value of leadership and understand that investments in talent bring performance improvement.</p>
+            <h2><?php the_field('heading'); ?></h2>
+            <?php the_field('about_summary'); ?>
           </div>
-
         </div>
       </div>
-
     </div>
 </section>
 
