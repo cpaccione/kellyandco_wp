f41b94424bb6e9bc12a9096e2009f006196f1965
Frost <cpaccione77@gmail.com>
2016-08-01 23:17:23 -0500

    script.js clean up

--- js/script.js
+++ js/script.js
@@ -5,30 +5,28 @@ jQuery(document).ready(function($) {
 
 	// Add bootstrap's 'img-responsive' class to all images
 
-    $('img').addClass('img-responsive');
+  $('img').addClass('img-responsive');
 
-    $('.svg').removeClass('img-responsive');
+  $('.svg').removeClass('img-responsive');
 
   // Animation for green text box on home page
 
-    $('.green-box').addClass('visible animated slideInLeft');
+  $('.green-box').addClass('visible animated slideInLeft');
 
 
   // tabs
 
-    $('.nav-pills a').click(function (e) {
+  $('.nav-pills a').click(function (e) {
       
-      e.preventDefault();
+    e.preventDefault();
     
-      $(this).tab('show');
+    $(this).tab('show');
 
-    })
+  });
 
   // dropdown on hover
 
-    $('.navbar [data-toggle="dropdown"]').bootstrapDropdownHover({
-        
-    });
+  $('.navbar [data-toggle="dropdown"]').bootstrapDropdownHover();
 
 });
 
