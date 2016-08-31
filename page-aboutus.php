7f91ac9d6e9efcf549645914eb517d671c4b9020
Frost <cpaccione77@gmail.com>
2016-08-31 08:31:12 -0500

    About us page - ACF fields
    Added ACF fields to the about us page.

--- page-aboutus.php
+++ page-aboutus.php
@@ -21,13 +21,11 @@ Template Name: About Us
       <div class="col-sm-12">
         <div class="about-us-wrap">
           <div class="page-title">
-            <h2 class="text-center">About Kelly &amp; Company</h2>  
+            <h2 class="text-center"><?php the_field('page_title'); ?></h2>  
           </div>
-          <p>Established in 1996, Kelly &amp; Company is a retained executive search firm with expertise in the identification, recruitment, assessment, selection, transition, assimilation and retention of senior talent within Human Resources, with a concentration in Retirement Services and Health &amp; Benefits.</p>
-          <p>Our clients include a select group of Financial Services firms in the Retirement Services industry, Employee Benefits Consulting &amp; Administration firms, Technology companies, Insurance companies and Fortune 500 corporations in their Employee Benefits department.</p>
-          <p>These clients appreciate the value of leadership and understand that investments in talent bring performance improvement.</p>
+          <?php the_field('page_copy'); ?>
         </div>
-      </div>  
+      </div>
     </div>
 
   </div>
