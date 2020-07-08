b1c3d3fb739b5df121a74ec6cca35d87d1236633
Chris Paccione <cpaccione77@gmail.com>
2020-07-08 12:47:02 -0500

    Removed Google map and centered contact info.
    This update was requested by the client.

---
+++ google-map-old.php
@@ -0,0 +1,214 @@
+<div class="col">
+        <div id="googleMap" style="width:100%; height: 600px;"></div>
+
+        <script>
+        function myMap() {
+            
+        const myLatLng = {
+            lat: 40.760409,
+            lng: -73.9820788
+        };
+
+
+        const mapProp= {
+            center: myLatLng,
+            zoom: 10,
+            disableDefaultUI: true,
+            styles:[
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ffffff"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dadada"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c9c9c9"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#003821"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  }
+]
+        };
+
+        const map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+        const contentString = '<div id="map-info">'+
+        '<p>Kelly and Company</p>'+
+        '<a href="https://www.google.com/maps/place/1350+6th+Ave+Fl+2,+New+York,+NY+10019/@40.7625614,-73.9799923,17z/data=!3m1!4b1!4m5!3m4!1s0x89c258f98fb5db9f:0x3b07a3bb67d72ff8!8m2!3d40.7625614!4d-73.9777983" target="_blank">Get directions.</a>'+
+        '</div>';
+
+        const infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+
+        const marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: 'Kelly and Company, New York, NY'
+        });
+
+        marker.addListener('click', function() {
+            infowindow.open(map, marker);
+        });
+
+        }
+
+
+
+        </script>
+    </div>
